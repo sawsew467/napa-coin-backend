0d792ba4f4114d0870b66f3487902cab916b181f
sawsew467 <bao.thang.1912@gmail.com>
2022-12-22 13:26:49 +0700

    create controller and errorHandler

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,5 +1,8 @@
+import { ErrorType } from "./../middlewares/errorHandler";
 import { Request, Response, NextFunction } from "express";
 import { User } from "../models/UserModel";
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 export const register = async (
   req: Request,
@@ -8,20 +11,51 @@ export const register = async (
 ) => {
   try {
     const user = await User.create(req.body);
-    console.log(user);
-    
+    const token = jwt.sign(user._id.toJSON(), "efwef");
     res.status(200).json({
       status: "success",
       data: {
+        token,
         user,
       },
     });
     res.json("User registered");
   } catch (err) {
-    
-    res.json(err);
+    next(err)
   }
 };
-export const login = (req: Request, res: Response, next: NextFunction) => {
-  res.json("User login");
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      //Error: Email is not correct
+      const err: ErrorType = new Error("Email is not valid");
+      err.status = 400;
+      return next(err);
+    }
+    if (req.body.password === user.password) {
+      const token = jwt.sign({ userId: user._id }, process.env.APP_SECRET);
+      const { fullname, email } = user;
+
+      res.status(200).json({
+        status: "success",
+        data: {
+          // userName: name,
+          user: {
+            fullname,
+            email,
+          },
+          token,
+        },
+      });
+    } else {
+      const err: ErrorType = new Error("Password is not correct");
+      err.status = 400;
+      return next(err);
+    }
+  } catch (error) {}
 };
