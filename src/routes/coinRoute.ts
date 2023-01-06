22814fd005e5fcb17dbfe61e6c769cf3e3a60ef9
le-toan-1 <85981385+le-toan-1@users.noreply.github.com>
2023-01-06 10:36:02 +0700

    api categories

--- src/routes/coinRoute.ts
+++ src/routes/coinRoute.ts
@@ -1,8 +1,9 @@
-import { getCoinLastest } from './../controllers/coinControllers';
+import { getCoinLastest, getCategories } from './../controllers/coinControllers';
 import express from 'express';
 
 const Router = express.Router();
 Router.route('/latest').get(getCoinLastest);
+Router.route('/categories').get(getCategories);
 // Router.route('/:userId').get(getUserById);
 
 module.exports = Router;
