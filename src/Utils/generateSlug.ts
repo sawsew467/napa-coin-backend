1fd99d20a0002b356f898fe79b82476326834ec8
sawsew467 <mac@MacBook-Pro.local>
2024-05-21 16:23:04 +0700

    feat: project

--- src/Utils/generateSlug.ts
+++ src/Utils/generateSlug.ts
@@ -1,3 +1,4 @@
+import { Project } from './../models/ProjectModel';
 import mongoose from 'mongoose';
 
 const slugify = require('slugify');
@@ -8,6 +9,19 @@ export async function generateUniqueSlug(doc: any) {
     let counter = 1;
 
     while (await mongoose.models.User.exists({ slug: uniqueSlug })) {
+        uniqueSlug = `${baseSlug}-${counter}`;
+        counter++;
+    }
+
+    return uniqueSlug;
+}
+
+export async function generateProjectUniqueSlug(doc: any) {
+    const baseSlug = slugify(`${doc.title}`, { lower: true, strict: true });
+    let uniqueSlug = baseSlug;
+    let counter = 1;
+
+    while (await mongoose.models.Project.exists({ slug: uniqueSlug })) {
         uniqueSlug = `${baseSlug}-${counter}`;
         counter++;
     }
---
+++ src/controllers/projectController.ts
@@ -0,0 +1,153 @@
+import { Request, Response, NextFunction } from 'express';
+const jwt = require('jsonwebtoken');
+
+import { User } from '../models/UserModel';
+import { Project } from '../models/ProjectModel';
+
+export const createProject = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const Authorization = req.header('authorization');
+        if (!Authorization) {
+            return res.status(400).json({
+                error: {
+                    statusCode: 400,
+                    status: 'error',
+                    message: 'Token is invalid',
+                },
+            });
+        }
+        const token = Authorization.replace('Bearer ', '');
+        const { userId } = jwt.verify(token, process.env.APP_SECRET);
+
+        const user = await User.findById(userId);
+
+        if (!user?.isAdmin) {
+            res.status(403).json({
+                status: 'error',
+                message: 'You are not allowed use this feature',
+            });
+        }
+
+        const project = await Project.create({
+            ...req.body,
+        });
+
+        res.status(200).json({
+            status: 'success',
+            data: project,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const getAllProject = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const projects = await Project.find({});
+
+        res.status(200).json({
+            status: 'success',
+            data: projects,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const getProjectBySlug = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const project = await Project.findOne({ slug: req.params.slug });
+
+        if (!project) {
+            res.status(500).json({
+                status: `Can't found project`,
+            });
+        }
+
+        res.status(200).json({
+            status: 'success',
+            data: project,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const editProjectById = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const Authorization = req.header('authorization');
+        if (!Authorization) {
+            return res.status(400).json({
+                error: {
+                    statusCode: 400,
+                    status: 'error',
+                    message: 'Token is invalid',
+                },
+            });
+        }
+
+        const token = Authorization.replace('Bearer ', '');
+        const { userId } = jwt.verify(token, process.env.APP_SECRET);
+
+        const user = await User.findById(userId);
+
+        if (!user?.isAdmin) {
+            res.status(403).json({
+                status: 'error',
+                message: 'You are not allowed to edit this profile',
+            });
+        }
+
+        const { id } = req.params;
+
+        const updateData = {
+            ...req.body,
+        };
+
+        const response = await Project.findByIdAndUpdate(id, updateData, { new: true, runValidator: true });
+
+        res.status(200).json({
+            status: 'success',
+            message: 'Edit project successfully',
+            data: response,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const deleteProjectById = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const Authorization = req.header('authorization');
+        if (!Authorization) {
+            return res.status(400).json({
+                error: {
+                    statusCode: 400,
+                    status: 'error',
+                    message: 'Token is invalid',
+                },
+            });
+        }
+        const token = Authorization.replace('Bearer ', '');
+        const { userId } = jwt.verify(token, process.env.APP_SECRET);
+
+        const user = await User.findById(userId);
+
+        if (!user?.isAdmin) {
+            res.status(403).json({
+                status: 'error',
+                message: 'You are not allowed use this feature',
+            });
+        }
+
+        const { id } = req.params;
+        await Project.findByIdAndDelete(id);
+
+        res.status(200).json({
+            status: 'success',
+            message: 'Delete project successfully',
+        });
+    } catch (error) {
+        next(error);
+    }
+};
